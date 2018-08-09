0f8107f269e402e3f84ce4407edbc974feee527b
PolGuixe <pol.guixe@gmail.com>
2018-08-09 19:58:39 +0200

    updated version for first publish

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'origenstudio:vulcan-material-ui',
-  version: '1.10.1_8',
+  version: '0.0.1',
   summary: 'Replacement for Vulcan (http://vulcanjs.org/) components using material-ui',
   git: 'https://github.com/ErikDakoda/vulcan-material-ui',
   documentation: 'README.md'
