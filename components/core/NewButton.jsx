05c91ca271762220b3d6514cb0e6021a79a40fb8
PolGuixe <pol.guixe@gmail.com>
2018-08-09 20:02:52 +0200

    improved code style

--- components/core/NewButton.jsx
+++ components/core/NewButton.jsx
@@ -1,44 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Components, replaceComponent } from 'meteor/vulcan:core';
-import { intlShape } from 'meteor/vulcan:i18n';
-import AddIcon from '@material-ui/icons/Add';
+import React from "react";
+import PropTypes from "prop-types";
+import { Components, replaceComponent } from "meteor/vulcan:core";
+import { intlShape } from "meteor/vulcan:i18n";
+import AddIcon from "@material-ui/icons/Add";
 
-
-const NewButton = ({
-                     className,
-                     collection,
-                     color = 'default',
-                     variant,
-                   }, { intl }) => (
-  
+const NewButton = (
+  { className, collection, color = "default", variant },
+  { intl }
+) => (
   <Components.ModalTrigger
     className={className}
-    component={<Components.TooltipIconButton titleId="datatable.new"
-                                             icon={<AddIcon/>}
-                                             color={color}
-                                             variant={variant}
-    />}
+    component={
+      <Components.TooltipIconButton
+        titleId="datatable.new"
+        icon={<AddIcon />}
+        color={color}
+        variant={variant}
+      />
+    }
   >
-    <Components.EditForm collection={collection}/>
+    <Components.EditForm collection={collection} />
   </Components.ModalTrigger>
 );
-
 
 NewButton.propTypes = {
   className: PropTypes.string,
   collection: PropTypes.object.isRequired,
-  color: PropTypes.oneOf(['default', 'inherit', 'primary', 'secondary']),
-  variant: PropTypes.string,
+  color: PropTypes.oneOf(["default", "inherit", "primary", "secondary"]),
+  variant: PropTypes.string
 };
-
 
 NewButton.contextTypes = {
   intl: intlShape
 };
 
+NewButton.displayName = "NewButton";
 
-NewButton.displayName = 'NewButton';
-
-
-replaceComponent('NewButton', NewButton);
+replaceComponent("NewButton", NewButton);
