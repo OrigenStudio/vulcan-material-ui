e90688e9ebb021835b6b4d6716559997e7c0f2d7
PolGuixe <pol.guixe@gmail.com>
2018-08-10 16:40:01 +0200

    added the css baseline and improved the code style

--- client/wrapWithMuiTheme.jsx
+++ client/wrapWithMuiTheme.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import { addCallback } from 'meteor/vulcan:core';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { getCurrentTheme } from '../modules/themes';
-import JssCleanup from '../components/theme/JssCleanup';
+import React from "react";
+import { addCallback } from "meteor/vulcan:core";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { getCurrentTheme } from "../modules/themes";
+import JssCleanup from "../components/theme/JssCleanup";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-
-function wrapWithMuiTheme (app) {
+function wrapWithMuiTheme(app) {
   const theme = getCurrentTheme();
   return (
     <MuiThemeProvider theme={theme}>
-      <JssCleanup>
-          {app}
-      </JssCleanup>
+      <CssBaseline />
+      <JssCleanup>{app}</JssCleanup>
     </MuiThemeProvider>
   );
 }
 
-
-addCallback('router.client.wrapper', wrapWithMuiTheme);
+addCallback("router.client.wrapper", wrapWithMuiTheme);
